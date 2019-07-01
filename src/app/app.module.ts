9fec5f9121ee4a572df208367cc7a0106c7a2fd6
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-01 09:29:40 -0300

    Criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
+import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
@@ -13,12 +13,13 @@ import { PessoasModule } from './pessoas/pessoas.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+
+    CoreModule,
 
     LancamentosModule,
     PessoasModule
