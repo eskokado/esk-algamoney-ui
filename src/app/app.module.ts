cf37a191b848107cf06ab7febf00c4000389c723
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-04 05:44:40 -0300

    Alterando o locale da aplicação para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { ToastrModule  } from 'ng6-toastr-notifications';
 
@@ -14,6 +16,8 @@ import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import {ConfirmationService} from 'primeng/api';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -36,7 +40,8 @@ import {ConfirmationService} from 'primeng/api';
 
   ],
   providers: [
-    ConfirmationService
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [AppComponent]
 })
