8e1988333b972115d1fc1a3c77b6d0bb3801dff8
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-04 09:17:48 -0300

    Implementando o serviço de listagem de categorias

---
+++ src/app/categorias/categoria.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  catetoriasUrl = 'http://localhost:8080/categorias';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  listarTodas(): Promise<any> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Basic ' + btoa('admin@algamoney.com' + ':' + 'admin'));
+    return this.http.get(`${this.catetoriasUrl}`, { headers })
+      .toPromise<any>()
+      .then((response) => {
+        console.log(response);
+        return response;
+      });
+  }
+}
