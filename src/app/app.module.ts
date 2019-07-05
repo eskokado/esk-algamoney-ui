3a0f19f7ab68db576629c64a6deaa2149b607f00
Edson Shideki Kokado <eskokado@gmail.com>
2019-07-05 17:54:56 -0300

    Refatorando as rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-
 import { PessoasModule } from './pessoas/pessoas.module';
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 
 registerLocaleData(localePt);
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'pessoas/nova', component: PessoaCadastroComponent },
-  { path: 'pessoas/:codigo', component: PessoaCadastroComponent },
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
 
 @NgModule({
   declarations: [
@@ -43,7 +25,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     FormsModule,
 
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
 
     HttpClientModule,
 
